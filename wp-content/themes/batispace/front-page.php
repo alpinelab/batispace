c756f0bb86ff339a17ee211c014bdd388905aa11
Michael Baudino <michael.baudino@alpine-lab.com>
2014-03-31 18:13:31 +0200

    Add front-page.php

---
+++ wp-content/themes/batispace/front-page.php
@@ -0,0 +1,14 @@
+<div class="banner">
+  Grandeur nature
+</div>
+
+<nav class="blocks">
+  <ul class="menu">
+    <li><a href="/metiers">Metiers</a></li>
+    <li><a href="/marques">Marques</a></li>
+    <li><a href="/infos-pratiques">Infos pratiques</a></li>
+    <li><a href="/posts">Actualités</a></li>
+    <li><a href="/contact">Contact</a></li>
+    <li><a href="/blog">Blog</a></li>
+  </ul>
+</nav>
