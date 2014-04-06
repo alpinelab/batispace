64e4eb231baa9314d7abd2aaaac238b570505e9b
Michael Baudino <michael.baudino@alpine-lab.com>
2014-04-06 11:07:37 +0200

    Create ribbon on upper-left corner of pages

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1f63c98b35f5ecaedee70493fe90f3ae');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd39aa5bacb9d673d803157bc8a771773');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
