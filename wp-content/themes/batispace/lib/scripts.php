77af830de536be354f5cea3c3d8fecfad90f1813
Michael Baudino <michael.baudino@alpine-lab.com>
2014-04-06 12:57:02 +0200

    Align ribbon on the right

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c478aaef1176924fc10cacae9902fe8c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '16f53a2d6b840d1849871b969e449cc3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
