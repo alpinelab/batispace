81c1c1942a41a4e7d4a04c36ab327418940aa328
Michael Baudino <michael.baudino@alpine-lab.com>
2014-04-06 12:38:55 +0200

    Change ribbon from an absolutely positioned element to a left-floated element.

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd39aa5bacb9d673d803157bc8a771773');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c478aaef1176924fc10cacae9902fe8c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
