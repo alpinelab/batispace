017b50e707e8be7c5d854e80c93390521fe7d283
Michael Baudino <michael.baudino@alpine-lab.com>
2014-04-15 00:01:58 +0200

    Add post featured image/video/carousel on archive page but in desktop version only.

--- wp-content/themes/batispace/templates/content.php
+++ wp-content/themes/batispace/templates/content.php
@@ -3,6 +3,15 @@
     <?php get_template_part('templates/entry-meta'); ?>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </header>
+
+  <div class="visible-md visible-lg">
+    <? if (has_post_thumbnail()) { // Featured image or featured video
+      echo the_post_thumbnail('large');
+    } else { // Carousel
+      echo do_shortcode('[carousel]');
+    } ?>
+  </div>
+
   <div class="entry-summary">
     <?php the_excerpt(); ?>
   </div>
