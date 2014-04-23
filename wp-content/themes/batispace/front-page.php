4325c974f77e173ef5cb1272a353978af735d92b
Michael Baudino <michael.baudino@alpine-lab.com>
2014-04-23 23:29:04 +0200

    Add carousel on homepage

--- wp-content/themes/batispace/front-page.php
+++ wp-content/themes/batispace/front-page.php
@@ -1,5 +1,9 @@
 <div class="home-banner">Grandeur nature</div>
 
+<div class="home-slider">
+  <?= do_shortcode('[image-carousel]') ?>
+</div>
+
 <nav class="home-blocks"> <?
   if (has_nav_menu('home_blocks')) {
     wp_nav_menu(array('theme_location' => 'home_blocks', 'menu_class' => 'blocks-wrapper'));
--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '282fd362344ed08696812d7028a344f6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd75d5f7bfe85eeef770c6a2dfcfd3ded');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
