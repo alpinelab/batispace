de3276658f3f469c6d242a555d762561d3b95f71
Michael Baudino <michael.baudino@alpine-lab.com>
2014-04-23 22:49:47 +0200

    Add a title on page Devis for desktop version

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8b5ed674c7a8b4a9f1e49cf6d8e3be16');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7c48bd15dae58a769e8f1475f3ab47c2');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
