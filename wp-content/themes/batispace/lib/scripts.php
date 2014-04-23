4bcf4a2b6873277b690176b166a91e4d07ea49d8
Michael Baudino <michael.baudino@alpine-lab.com>
2014-04-23 23:13:39 +0200

    Add background images to home blocks and banner

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7c48bd15dae58a769e8f1475f3ab47c2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '282fd362344ed08696812d7028a344f6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
