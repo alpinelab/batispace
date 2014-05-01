46151b9091154b97e1de421c5ff8e48666341094
Michael Baudino <michael.baudino@alpine-lab.com>
2014-05-02 00:22:01 +0200

    Rename Categorie CPT to Produits

--- wp-content/themes/batispace/types/realisations.php
+++ wp-content/themes/batispace/types/realisations.php
@@ -3,21 +3,42 @@
 function create_post_type() {
   register_post_type('realisation',
     array(
-      'labels' => array(
-        'name' => __('Realisations'),
+      'labels'      => array(
+        'name'          => __('Realisations'),
         'singular_name' => __('Realisation')
       ),
-      'public' => true,
+      'public'      => true,
       'has_archive' => true,
-      'supports' => array('title', 'editor', 'thumbnail')
+      'rewrite'     => array('slug' => 'realisations'),
+      'supports'    => array('title', 'editor', 'thumbnail')
     )
   );
 }
 add_action('init', 'create_post_type');
 
 function create_taxonomies() {
-  $categories_args = array('hierarchical' => true);
-  register_taxonomy('category', array('realisation'), $categories_args );
+  $produits_labels = array(
+    'name'               => _x('Produits', 'post type general name',      'batispace'),
+    'singular_name'      => _x('Produit', 'post type singular name',      'batispace'),
+    'menu_name'          => _x('Produits', 'admin menu',                  'batispace'),
+    'name_admin_bar'     => _x('Produit', 'add new on admin bar',         'batispace'),
+    'add_new'            => _x('Ajouter un nouveau', 'realisation',       'batispace'),
+    'add_new_item'       => __('Ajouter un nouveau Produit',              'batispace'),
+    'new_item'           => __('Nouveau Produit',                         'batispace'),
+    'edit_item'          => __('Modifier le Produit',                     'batispace'),
+    'view_item'          => __('Voir le Produit',                         'batispace'),
+    'all_items'          => __('Tous les Produits',                       'batispace'),
+    'search_items'       => __('Chercher des Produits',                   'batispace'),
+    'parent_item_colon'  => __('Produits parents:',                       'batispace'),
+    'not_found'          => __('Aucun Produit trouvé.',                   'batispace'),
+    'not_found_in_trash' => __('Aucun Produit trouvé dans la Corbeille.', 'batispace')
+  );
+  $produits_args = array(
+    'hierarchical'  => true,
+    'rewrite'       => array('slug' => 'produits'),
+    'labels'        => $produits_labels
+  );
+  register_taxonomy('produit', array('realisation'), $produits_args);
 
   $materiaux_labels = array(
     'name'               => _x('Matériaux', 'post type general name',      'batispace'),
@@ -31,13 +52,14 @@ function create_taxonomies() {
     'view_item'          => __('Voir le Matériau',                         'batispace'),
     'all_items'          => __('Tous les Matériaux',                       'batispace'),
     'search_items'       => __('Chercher des Matériaux',                   'batispace'),
-    'parent_item_colon'  => __('Materiaux parents:',                       'batispace'),
+    'parent_item_colon'  => __('Matériaux parents:',                       'batispace'),
     'not_found'          => __('Aucun Matériau trouvé.',                   'batispace'),
     'not_found_in_trash' => __('Aucun Matériau trouvé dans la Corbeille.', 'batispace')
   );
   $materiaux_args = array(
     'hierarchical'  => true,
-    'labels'        => $materiaux_labels
+    'labels'        => $materiaux_labels,
+    'rewrite'       => array('slug' => 'materiaux')
   );
 
   register_taxonomy('materiau', array('realisation'), $materiaux_args);
