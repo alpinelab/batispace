c28b0e2152d02b036875b2cc46c8885e3e3d1af2
Michael Baudino <michael.baudino@alpine-lab.com>
2014-05-05 23:57:33 +0200

    Add Réalisations block on homepage

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5ca1312cbba2ddd4fe52a7550c5b4340');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7b2c7e7012cdb78e1bb4ba8d090bfb63');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
