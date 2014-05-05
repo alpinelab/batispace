983630cffe3df76a436e513c3de55deb300eba3a
Michael Baudino <michael.baudino@alpine-lab.com>
2014-05-05 23:42:47 +0200

    Change blocks proportions

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4dd9a04ca40632a790d91b97ed90ce22');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5ca1312cbba2ddd4fe52a7550c5b4340');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
