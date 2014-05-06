d6d86715a195195da01ccaa0371596a60ec7b79b
Michael Baudino <michael.baudino@alpine-lab.com>
2014-05-06 22:48:14 +0200

    Change footer background color

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c17feb04a907b693f612cb33dfb93a41');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bb28ee342cbcd1abc40fd49869194f1a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
