8c7b501af43b37f5d8d0721c64b0c210e9b3bdaa
Michael Baudino <michael.baudino@alpine-lab.com>
2014-05-06 22:46:09 +0200

    Add style to ribbons

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7b2c7e7012cdb78e1bb4ba8d090bfb63');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c17feb04a907b693f612cb33dfb93a41');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
