4d54bf0af6a42d3a8fe77b4aec23466aa506d66d
Michael Baudino <michael.baudino@alpine-lab.com>
2014-05-19 00:34:54 +0200

    Change some style in Realisations single page

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b783b26de481e3ff1bac9ac7548583da');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6b856593b78fea8952312026e16e6dc0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
