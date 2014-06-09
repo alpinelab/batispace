f2dc9cd8d0bf6da137e057e0c52dfbdca76a3a95
Michael Baudino <mike@kamehouse.biz>
2014-06-09 18:48:53 +0200

    Add real pictures in home blocks

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8b65a71027e14aee5b56be5d9ecdfdae');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5aa8b28b9e2738ee8113afd770f7aa66');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
