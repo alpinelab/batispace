ae7f3c308cd8427e2187af3e00f623bb69a17f09
Michael Baudino <mike@kamehouse.biz>
2014-06-29 15:47:18 +0200

    Visual changes for Mathilde

--- wp-content/themes/batispace/lib/scripts.php
+++ wp-content/themes/batispace/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8322f31bec21d03894d08c0d7a2e4c3c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7efdaf3d61973db014b9e9c3326f2ba3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
